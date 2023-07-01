86dc4d04290b1da1c1747170f6431263d99730a8
gustavolopesnobrega <gustavo.lopes.nobrega@gmail.com>
2023-07-01 16:35:16 -0300

    testing build

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -16,7 +16,7 @@ OAuth2_client.setCredentials({ refresh_token: refreshToken});
 const acessToken = OAuth2_client.getAccessToken();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'gmail.com',
     port: 465,
     secure: true,
     auth: {
@@ -49,7 +49,7 @@ const mailer = ({ senderMail, name, text }) => {
     });
 };
 
-export default async (req: any, res: any) => { 
+export default async (req, res) => { 
     const { senderMail, name, content } = req.body;
     if (senderMail === '' || name === '' || content === ''){
         res.status(403).send();
